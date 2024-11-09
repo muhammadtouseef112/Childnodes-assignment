c3c4928502430ee4eff6a004ae343e07d135ee57
@Muhammad Touseef14 <immuhammadtouseef@gmail.com>
2024-11-10 01:24:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+alert("you will find your answer on console")
+function nav(){
+var element = document.childNodes[1].childNodes[2].childNodes[1]
+console.log(element);
+alert("see your find word in console")
+}
+function ul(){
+    var element2 = document.childNodes[1].childNodes[2].childNodes[5]
+    console.log(element2)
+}
+function list(){
+    var element3 = document.childNodes[1].childNodes[2].childNodes[9].childNodes[0]
+    console.log(element3)
+}
+function list2(){
+    var element4 = document.childNodes[1].childNodes[2].childNodes[13].childNodes[0]
+    console.log(element4)
+}
+function anchor(){
+    var element5 = document.childNodes[1].childNodes[2].childNodes[17].childNodes[0]
+    console.log(element5)
+}
+function anchor1(){
+    var element5 = document.childNodes[1].childNodes[2].childNodes[21].childNodes[0]
+    console.log(element5)
+}
+function heading(){
+    var element5 = document.childNodes[1].childNodes[2].childNodes[25].childNodes[1].childNodes[0]
+    console.log(element5)
+}
+
+function image(){
+    var element5 = document.childNodes[1].childNodes[2].childNodes[25].childNodes[5].childNodes[1]
+    console.log(element5)
+}
